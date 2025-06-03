e079ca635fa6df19efe3f0a7af03a7644b171f3f
ramirouf <ramirouf@gmail.com>
2025-06-03 15:26:46 -0300

    Se cambia entre tema claro y oscuro cambiando atributo data-bs-theme en vez de cambiar de stylesheet

--- crud/static/js/theme.js
+++ crud/static/js/theme.js
@@ -1,16 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const themeLinks = {
-    light: "https://bootswatch.com/5/lumen/bootstrap.css",
-    dark: "https://bootswatch.com/5/darkly/bootstrap.min.css",
-  };
   const themeDropdownItems = document.querySelectorAll("[data-theme]");
-  const themeLink = document.querySelector('link[rel=stylesheet][href*="bootswatch"]');
+  const html = document.documentElement;
 
   themeDropdownItems.forEach((item) => {
     item.addEventListener("click", function (e) {
       e.preventDefault();
       const selected = this.getAttribute("data-theme");
-      themeLink.href = themeLinks[selected];
+      html.setAttribute("data-bs-theme", selected);
       fetch(`/set_theme/${selected}`, { method: "POST" });
     });
   });
